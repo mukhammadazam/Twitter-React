8aee11d2040c71de938902ac5b59ec77cb81ef4a
mukhammadazam <rizayevmuhammadazam9@gmail.com>
2023-03-29 19:21:15 +0500

    right sahifa start

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import LeftSahifa from './twitter/LeftSahifa';
 import { Routes, Route } from 'react-router-dom';
+import Right from './right/Right';
 function App() {
   return (
-    <div className='container'>
+    <div className='container pt-3 d-flex justify-content-lg-between'>
       <LeftSahifa />
       <Routes>
         <Route path='/' element={<h1>Home</h1>} />
       </Routes>
+      <Right/>
     </div>
 
   );
---
+++ src/right/Right.jsx
@@ -0,0 +1,13 @@
+import './Right.scss'
+const Right = () => {
+  return (
+    <div className="right">
+<input type="text" placeholder="Search Twitter" className="right__input" />
+<div className="right__enner">
+    
+</div>
+    </div>
+  )
+}
+
+export default Right
